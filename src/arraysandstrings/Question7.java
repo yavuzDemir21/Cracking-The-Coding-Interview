bba37d806d621e185d9adfa17f86ff39a6f361f7
yavuz.demir <yavuz.demir@trendyol.com>
2021-02-08 10:21:52 +0300

    add ctci arrays and strings Rotate Matrix question

---
+++ src/arraysandstrings/Question7.java
@@ -0,0 +1,54 @@
+package arraysandstrings;
+/*
+Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes, write a method to rotate the
+image by 90 degrees. Can you do this in place?
+
+*/
+
+public class Question7 {
+    public static void main(String[] args) {
+        int[][] matrix = {{1,1,1}, {0,0,0}, {2,2,2}};
+        printMatrix(matrix);
+        System.out.println();
+        rotateMatrix(matrix);
+        printMatrix(matrix);
+        System.out.println();
+        rotateMatrix(matrix);
+        printMatrix(matrix);
+        System.out.println();
+        rotateMatrix(matrix);
+        printMatrix(matrix);
+        System.out.println();
+        rotateMatrix(matrix);
+        printMatrix(matrix);
+    }
+
+    //Assume clockwise rotation
+    private static void rotateMatrix(int[][] matrix){
+
+        for(int i=0; i<matrix.length/2; i++){
+            int len = matrix[0].length-i-1;
+            for(int j=i; j<len; j++){
+
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[len-j][i];
+                matrix[len-j][i] = matrix[len-i][len-j];
+                matrix[len-i][len-j] = matrix[j][len-i];
+                matrix[j][len-i] = temp;
+
+
+            }
+        }
+
+    }
+
+    private static void printMatrix(int[][] matrix){
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
